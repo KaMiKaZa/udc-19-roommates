6f15d918c702d838eebc25401f0829a2adea0fe4
Stanislav <kamikaza1997@gmail.com>
2021-09-11 14:51:51 +0500

    Refactored PlayerCharacter, added ability to end the turn by pressing Space

--- Assets/Scripts/Characters/PlayerCharacter.cs
+++ Assets/Scripts/Characters/PlayerCharacter.cs
@@ -1,10 +1,9 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(PlayerActor))]
 public class PlayerCharacter : Character {
   private PlayerActor playerActor;
-
-  private Vector2? chosenMove = null;
 
   protected override void Awake() {
     base.Awake();
@@ -12,25 +11,20 @@ public class PlayerCharacter : Character {
     playerActor = GetComponent<PlayerActor>();
   }
 
-  private void Update() {
-    // TODO: player can select movement direction before the actual turn
-
-    if (!IsMyTurn) {
-      chosenMove = null;
-
-      return;
-    }
-
-    if (playerActor.move != Vector2.zero && playerActor.move.magnitude <= 1f) {
-      if (GridManager.Instance.IsInputValid(transform.position, playerActor.move)) {
-        chosenMove = playerActor.move;
-      }
-    }
-  }
-
   public override Vector2? GetMove() {
     UIManager.Instance.TurnInfoText.text = "Your turn";
 
-    return chosenMove;
+    if (playerActor.move != Vector2.zero && playerActor.move.magnitude <= 1f) {
+      if (GridManager.Instance.IsInputValid(transform.position, playerActor.move)) {
+        return playerActor.move;
+      }
+    }
+
+    if (playerActor.turnEnd) {
+      // TODO: may skip more than one turn!
+      return Vector2.zero;
+    }
+
+    return null;
   }
 }
